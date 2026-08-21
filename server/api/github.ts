6de321549f1e0db857dce35c564c0e4327f19ddb
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-21 10:35:11 +0800

    Update github.ts

--- server/api/github.ts
+++ server/api/github.ts
@@ -30,16 +30,4 @@ async function oauth(code: string) {
 }
 
 
-// src/platforms/GitHub.ts
-async listBranches(meta: MetaData): Promise<Branch[]> {
-  const { user, repo } = meta
-  const response = await this.request(
-    `repos/${user}/${repo}/branches?per_page=100`
-  )
-  return response.map((b: any) => ({
-    name: b.name,
-    protected: b.protected,
-  }))
-}
-
 export default createCodeHandler(oauth)
